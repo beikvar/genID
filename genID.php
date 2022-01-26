33289e73e161899e286472295ef018832a358487
luqaska <83518257+luqaska@users.noreply.github.com>
2022-01-26 18:19:34 +0000

    Update genID.php

--- genID.php
+++ genID.php
@@ -1,14 +1,9 @@
 <?php function genID($n) {
   $chars="0123456789abcdefghijklmnopqrstuvwxyz";
   $id = ''; $thing=0;
-  while($thing==0){
-    for ($i=0; $i<$n; $i++) {
-      $r = rand(0, strlen($chars)-1);
-      $id .= $chars[$r];
-    }
-    if(strpos(file_get_contents("db/posts/index.txt"),"$id\n") === false){
-      $thing=1;
-    }
+  for ($i=0; $i<$n; $i++) {
+    $r = rand(0, strlen($chars)-1);
+    $id .= $chars[$r];
   }
   return $id;
 }
